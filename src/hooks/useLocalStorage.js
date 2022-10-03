3b883b3c51b9938254966450897d37273103e1da
Mikiman <jurja.dumitru@gmail.com>
2022-10-03 22:35:18 +0300

    Started to move some logic into custom hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,1 +1,26 @@
+import { useState, useEffect } from "react";
 
+export const useLocalStorage = () => {
+  const defaultNote = [
+    {
+      title: "Note title",
+      content: "Note content",
+    },
+  ];
+
+  const getNoteValue = () => {
+    const saved = window.localStorage.getItem("note");
+    const savedNote = JSON.parse(saved);
+    return savedNote || defaultNote;
+  };
+
+  const useLocalStorage = () => {
+    const [notes, setNotes] = useState(getNoteValue);
+
+    useEffect(() => {
+      window.localStorage.setItem("note", JSON.stringify(notes));
+    }, [notes]);
+
+    return [notes, setNotes];
+  };
+};
