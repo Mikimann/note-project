28d3a423bd8ee3fb4231ca06835ee218b84ad3a6
Mikiman <jurja.dumitru@gmail.com>
2023-01-27 18:47:43 +0200

    Add new path to router

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
+import Profile from '../pages/Account/Account';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="home" element={<Home />} />
       <Route path="/" element={<Home />} />
+      <Route path="account" element={<Profile />} />
     </Routes>
   );
 };
