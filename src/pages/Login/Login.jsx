d1d1d5cef829815a67dd51b9af1a53d03c5a694e
Mikimann <jurja.dumitru@gmail.com>
2023-02-14 13:38:03 +0200

    Add Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router';
+import { useAuth } from '../../providers/useAuth';
+
 export const Login = () => {
-  return <div></div>;
+  const [user, setUser] = useState('');
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const redirectPath = location.state?.path || '/account';
+
+  const handleLogin = () => {
+    auth.login(user);
+    navigate(redirectPath, { replace: true });
+  };
+  return (
+    <div>
+      <label htmlFor="">
+        Username:{''}
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
+        <br />
+        Password:
+        <input type="password" name="" id="" />
+      </label>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
