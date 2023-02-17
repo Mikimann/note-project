a2428c061428e2325c3f51c224f55222f80d47d4
Mikimann <jurja.dumitru@gmail.com>
2023-02-17 16:01:28 +0200

    Add routes

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Profile from '../pages/Account/Account';
+import Login from '../pages/Login/Login';
+import { ProtectedRoute } from '../containers/ProtectedRoute';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="home" element={<Home />} />
       <Route path="/" element={<Home />} />
-      <Route path="account" element={<Profile />} />
+      <Route
+        path="account"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 };
